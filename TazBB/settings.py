6ce6790446f6f80c6ad1a0136997a2ffcd6baaf4
Taziamoma <taziamoma@gmail.com>
2021-06-27 00:11:41 -0500

    Added avatars

--- TazBB/settings.py
+++ TazBB/settings.py
@@ -132,6 +132,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = "index"
