f3231decb492352c169c4a9c769d8078988133a3
Taziamoma <taziamoma@gmail.com>
2021-07-01 13:59:30 -0500

    Updated settings.py for more secure settings

--- TazBB/settings.py
+++ TazBB/settings.py
@@ -25,11 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = 'g3_&$k5*)b7ygm5#)*!u8^6ct1ell#8uq1z4oay+il!um3kpiy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tazbb.herokuapp.com','127.0.0.1']
 
